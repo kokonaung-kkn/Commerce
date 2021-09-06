f899cbfcc3b812912dabcdb6ad664c50062d5d14
Ko Ko Naung <kokonaung.kk53@gmail.com>
2021-09-06 12:04:43 +0630

    Changing Directory

--- classes/Helpers/HTTP.php
+++ classes/Helpers/HTTP.php
@@ -3,7 +3,7 @@
 namespace Helpers;
 
 class HTTP{
-    static $base = "http://localhost/commerce/php";
+    static $base = "http://localhost/commerce";
 
     static function redirect($path, $query=0){
         $url = static::$base.$path;
